20b32bca674de1122d3acf478f4d0a5170f85695
charjia999 <charjia77@gmail.com>
2026-04-01 13:32:45 +1000

    add level selection feature

---
+++ levels.js
@@ -0,0 +1,32 @@
+const LEVELS = [
+  [
+    { id:"zf", name:"张飞", shape:[[1],[1]], x:0, y:0, img:"./images/zhangfei.jpg", rot: 0 },
+    { id:"zy", name:"赵云", shape:[[1],[1]], x:3, y:0, img:"./images/zhaoyun.jpg", rot: 0 },
+    { id:"mc", name:"马超", shape:[[1],[1]], x:0, y:2, img:"./images/machao.jpg", rot: 0 },
+    { id:"hz", name:"黄忠", shape:[[1],[1]], x:3, y:2, img:"./images/huangzhong.jpg", rot: 0 },
+    { id:"gy", name:"关羽", shape:[[1,1]], x:1, y:2, img:"./images/guanyu.jpg", rot: 0 },
+    { id:"cc", name:"曹操", shape:[[1,1],[1,1]], x:1, y:0, img:"./images/caocao.jpg", rot: 0 },
+    { id:"b1", name:"兵", shape:[[1]], x:1, y:3, img:"./images/bing6.jpg", rot: 0 },
+    { id:"b2", name:"兵", shape:[[1]], x:2, y:3, img:"./images/bing7.jpg", rot: 0 },
+    { id:"b3", name:"兵", shape:[[1]], x:0, y:4, img:"./images/bing8.jpg", rot: 0 },
+    { id:"b4", name:"兵", shape:[[1]], x:3, y:4, img:"./images/bing9.jpg", rot: 0 }
+  ],
+
+  [
+    { id:"zf", name:"张飞", shape:[[1],[1]], x:0, y:0, img:"./images/zhangfei.jpg", rot: 0 },
+    { id:"zy", name:"赵云", shape:[[1],[1]], x:3, y:0, img:"./images/zhaoyun.jpg", rot: 0 },
+    { id:"mc", name:"马超", shape:[[1],[1]], x:0, y:3, img:"./images/machao.jpg", rot: 0 },
+    { id:"hz", name:"黄忠", shape:[[1],[1]], x:3, y:3, img:"./images/huangzhong.jpg", rot: 0 },
+    { id:"gy", name:"关羽", shape:[[1,1]], x:1, y:2, img:"./images/guanyu.jpg", rot: 0 },
+    { id:"cc", name:"曹操", shape:[[1,1],[1,1]], x:1, y:0, img:"./images/caocao.jpg", rot: 0 },
+    { id:"b1", name:"兵", shape:[[1]], x:1, y:3, img:"./images/bing6.jpg", rot: 0 },
+    { id:"b2", name:"兵", shape:[[1]], x:2, y:3, img:"./images/bing7.jpg", rot: 0 },
+    { id:"b3", name:"兵", shape:[[1]], x:0, y:2, img:"./images/bing8.jpg", rot: 0 },
+    { id:"b4", name:"兵", shape:[[1]], x:3, y:2, img:"./images/bing9.jpg", rot: 0 }
+  ],
+
+  [
+    // 第n关数据
+  ]
+
+];
